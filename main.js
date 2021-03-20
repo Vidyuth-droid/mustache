44751440a2434764131f56ae73c17a9e02a8dc2e
Vidyuth-droid <72655577+Vidyuth-droid@users.noreply.github.com>
2021-03-20 17:41:43 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
+NoseX=0;
+NoseY=0;
 function preload(){
-
+    muctache = loadImage(' https://i.postimg.cc/HkwWBgt1/m-removebg-preview.png');
 }
 function setup(){
 canvas=createCanvas(300,300);
@@ -9,13 +11,14 @@ video.size(300,300);
 video.hide();
 poseNet=ml5.poseNet(video,modelLoaded);
 poseNet.on("pose",gotPoses);
+
 }
 function modelLoaded(){
     console.log("poseNet is initialized");
 }
 function draw(){
 image(video,0,0,300,300);
-
+image(muctache,NoseX,NoseY,50,50);
 }
 function take_snapshot(){
   save("circus.png");
@@ -24,7 +27,10 @@ function gotPoses(results){
     if (results.length>0){
         console.log(results);
         console.log("Nose X= "+results[0].pose.nose.x);
-        console.log("Nose Y= "+results[0].pose.nose.y);
+        console.log("Nose Y= "+results[0].pose.nose.y); 
+        NoseX=results[0].pose.nose.x-20;
+        NoseY=results[0].pose.nose.y;
+
     }
 }
 
